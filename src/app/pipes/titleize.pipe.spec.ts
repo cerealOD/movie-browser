bfdf344222aab3e37b394b77d9261c7a5a6c4326
mankahuszar <97175874+cerealOD@users.noreply.github.com>
2025-10-07 11:22:33 +0200

    Delete spec files, only leave footer for test

--- src/app/pipes/titleize.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TitleizePipe } from './titleize.pipe';
-
-describe('TitleizePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TitleizePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
